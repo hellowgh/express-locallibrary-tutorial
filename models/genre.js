e53a608ed5b6f398861c25018a53e426fb493461
hellowgh <hellowgh@qq.com>
2024-01-16 05:47:58 +0800

    修正genre list

--- models/genre.js
+++ models/genre.js
@@ -3,15 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    maxLength: 100,
-  }
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
 });
 
-GenreSchema.virtual('url', function () {
+// Virtual for this genre instance URL.
+GenreSchema.virtual('url').get(function () {
   return `/catalog/genre/${this._id}`;
 });
 
+// Export model.
 module.exports = mongoose.model('Genre', GenreSchema);
